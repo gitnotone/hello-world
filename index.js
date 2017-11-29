24d9a9ac7ef442f1b09cd798ce53b552d75bb68b
git.notone <hw1208.ahn@gmail.com>
2017-11-29 10:07:03 -0300

    agregar estatico

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require('express'); // Cargue el módulo expreso instalado en la variable (express)
-var app = express(); //Ejecute express para inicializar el objeto de la aplicación
+// index.js
+﻿
+﻿var express = require('express');
+var app = express();
 
-app.get('/',function (req,res) { // Si obtiene una solicitud de 'get' en '/'
-res.send('Hello World!'); // Enviar 'Hello World!"
-});
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function(){ //Usa el puerto 3000
-console.log('Server On!'); //Este es el mensaje que se mostrará en la ventana de la consola cuando el servidor se esté ejecutando
+app.listen(3000, function(){
+ console.log('Server On!');
 });
