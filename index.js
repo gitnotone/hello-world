4acb4483714fc5a724ca2b7e6861803c17cc9c50
git.notone <hw1208.ahn@gmail.com>
2017-11-30 09:31:20 -0300

    ahora heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
  console.log('Server On!');
 });
